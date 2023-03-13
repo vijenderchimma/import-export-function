d5bc3f3f9573277f2cf76a67058bb1d85cd594a0
vijenderchimma <vijenderchimma424@gmail.com>
2023-03-13 11:48:05 +0000

    import and export a function

---
+++ exportFunction.js
@@ -0,0 +1,4 @@
+function new_function() {
+  return "This is a Function";
+}
+module.exports = new_function;
---
+++ importFunction.js
@@ -0,0 +1,2 @@
+const new_function = require("./exportFunction");
+console.log(new_function());
